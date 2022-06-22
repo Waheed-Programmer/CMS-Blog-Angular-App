e942f102d3c30b701eb1593c723445481557cb3c
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-22 16:31:18 +0500

    set list of recipe

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -4,16 +4,21 @@ import { Recipe } from '../recipe.Model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
-  recipe:Recipe[] = [
-    new Recipe('A test recipe', 'Simply Data','https://www.acouplecooks.com/wp-content/uploads/2020/06/Caesar-Salad-022-735x919.jpg')
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'Simply Data',
+      'https://www.acouplecooks.com/wp-content/uploads/2020/06/Caesar-Salad-022-735x919.jpg'
+    ),
+    new Recipe(
+      'A Demo recipe',
+      'Simply Data',
+      'https://thumbs.dreamstime.com/z/recipe-word-text-green-leaf-logo-icon-design-white-background-suitable-card-typography-143638205.jpg'
+    ),
   ];
-  constructor() { }
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 }
