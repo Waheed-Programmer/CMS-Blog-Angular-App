0d391219bb12dd14356fd4568102620fad6acb23
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-22 15:41:48 +0500

    add new recipe model

---
+++ src/app/recipe/recipe.Model.ts
@@ -0,0 +1,10 @@
+export class recipe{
+public name: string ;
+public desc: string;
+public imagpath: string;
+constructor(name:string , desc: string , imagepath: string){
+    this.name = name;
+    this.desc = desc;
+    this.imagpath =imagepath;
+}
+}
