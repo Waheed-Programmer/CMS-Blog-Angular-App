8c72f3e98457ba840cb69a0d3e12a88b44149aea
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-28 10:13:16 +0500

    create new method for inspections

--- src/app/services/inspection-api-servvice.service.ts
+++ src/app/services/inspection-api-servvice.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InspectionAPIServviceService {
+  readonly InspectionApIUrl = "https://localhost:7185/api";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  //Inspections
+
+  getInspectionList():Observable<any[]>{
+    return this.http.get<any>(this.InspectionApIUrl + '/Inspections')
+  }
+
+  addInspection(data:any){
+    return this.http.post(this.InspectionApIUrl + '/Inspections',data)
+  }
+  updateInspection(id:number|string, data:any){
+    return this.http.put(this.InspectionApIUrl + `/Inspections/${id}`, data)
+  }
+
+  deleteInspection(id:number|string){
+    return this.http.delete(this.InspectionApIUrl + `/Inspection/${id}`)
+  }
+
 }
