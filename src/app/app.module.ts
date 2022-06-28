e3a7e14fef76095c19c05e4c7beade902729e8df
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-28 09:50:15 +0500

    inject all component and services in app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InspectionComponent } from './inspection/inspection.component';
 import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
 import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
+import { InspectionAPIServviceService } from './services/inspection-api-servvice.service';
 
 
 @NgModule({
@@ -14,13 +15,13 @@ import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add
     InspectionComponent,
     ShowInspectionComponent,
     AddEditInspectionComponent,
-    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [InspectionAPIServviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/inspection-api-servvice.service.ts
+++ src/app/services/inspection-api-servvice.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class InspectionApiServiceService {
+export class InspectionAPIServviceService {
 
   constructor() { }
 }
