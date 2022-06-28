0bbfbc4d850849048da495573e6d09bf5323c76f
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-28 09:42:34 +0500

    Add all inspection relevent component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InspectionComponent } from './inspection/inspection.component';
+import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
+import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    InspectionComponent,
+    ShowInspectionComponent,
+    AddEditInspectionComponent,
     
   ],
   imports: [
