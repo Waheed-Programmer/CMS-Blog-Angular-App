61a176eaed2874773c4e59de0973d6dc2d8eef4b
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-07-19 11:38:00 +0500

    ngIf apply

--- src/app/structral-directives/structral-directives.component.ts
+++ src/app/structral-directives/structral-directives.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructralDirectivesComponent implements OnInit {
 
+  displayName = false;
   constructor() { }
 
   ngOnInit(): void {
