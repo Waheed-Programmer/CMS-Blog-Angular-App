4dcaebd8f63e7def6ce27f565eed368e5cedc086
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-07-19 12:22:19 +0500

    Use  input decorator

--- src/app/structral-directives/structral-directives.component.ts
+++ src/app/structral-directives/structral-directives.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-structral-directives',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructralDirectivesComponent implements OnInit {
 
-  displayName = false;
+  @Input('parentData') public name:any
+  displayName = true;
+  public color = "Yellow";
+  public colors = ["red" ,"Yellow","Green","Mehroon"]
   constructor() { }
 
   ngOnInit(): void {
