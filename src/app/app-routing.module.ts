83fc6f7e8cb41abd9a3e5d182586cf080452f8d2
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-08-23 09:31:34 +0500

    Add ng bootstrap element in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StructralDirectivesComponent } from './structral-directives/structral-directives.component';
+
 
 const routes: Routes = [
-  {path:"", component:StructralDirectivesComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InspectionComponent } from './inspection/inspection.component';
-import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
-import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
+
+
+
 import { InspectionAPIServviceService } from './services/inspection-api-servvice.service';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { FormsModule } from '@angular/forms';
-import { StructralDirectivesComponent } from './structral-directives/structral-directives.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    InspectionComponent,
-    ShowInspectionComponent,
-    AddEditInspectionComponent,
-    StructralDirectivesComponent,
+
   ],
   imports: [
-    NgbModule,
+
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+
   ],
   providers: [InspectionAPIServviceService],
   bootstrap: [AppComponent]
