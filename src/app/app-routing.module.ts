a0af45a2aea1da457d7a24c853f4e158ec0b40de
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-09-01 19:53:42 +0500

    eror solve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,7 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
-  {path:'**',component:PagenotfoundComponent}
+  { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
